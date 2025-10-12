09c01f15b7a6a910edf7aadf6d7fe3debee53abb
Presley Gonçalves da Silva <presleygs.1607@gmail.com>
2025-10-12 11:34:58 -0300

    Update qpanel.service.js
    sasar,

--- src/qpanel/qpanel.service.js
+++ src/qpanel/qpanel.service.js
@@ -86,20 +86,21 @@ async function criarUsuarioQpanel(nome, email, whatsapp, packageId, serverPackag
     }
 
     console.log('🛠 Criando usuário no QPanel...');
-  const response = await axios.post(`${API_URL}/customer/create`, {
+const response = await axios.post(`${API_URL}/customer/create`, {
   userId: USER_ID,
   packageId: serverPackageId,
   username: username,
   password: password,
   name: nome,
-  email: email,       // aqui vai o email do cliente
-  whatsapp: whatsapp, // aqui vai o número do WhatsApp
+  email: whatsapp,
+  whatsapp: email,
 }, {
   headers: {
     Authorization: `Bearer ${API_TOKEN}`,
     'Content-Type': 'application/json',
   }
 });
+
     if (response.data && response.data.username) {
       console.log('✅ Usuário criado no QPanel:', response.data);
 
